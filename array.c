243f477ed51b0097fc300ea68a03810842773df9
kalyankrishna768 <132119666+kalyankrishna768@users.noreply.github.com>
2023-06-09 14:28:49 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    // Declare an array of integers
+    int numbers[5];
+
+    // Initialize the array with values
+    numbers[0] = 10;
+    numbers[1] = 20;
+    numbers[2] = 30;
+    numbers[3] = 40;
+    numbers[4] = 50;
+
+    // Access and print array elements
+    printf("Array elements:\n");
+    printf("%d\n", numbers[0]);
+    printf("%d\n", numbers[1]);
+    printf("%d\n", numbers[2]);
+    printf("%d\n", numbers[3]);
+    printf("%d\n", numbers[4]);
+
+    return 0;
+}
